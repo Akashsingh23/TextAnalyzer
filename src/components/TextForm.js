069f742b02b4eaec859de3cbf2d7933a52ab7de4
Akashsingh23 <akashthakur7038@gmail.com>
2025-01-31 16:56:16 +0530

    Initial commit

---
+++ src/components/TextForm.js
@@ -0,0 +1,59 @@
+import React, {useState} from 'react'
+
+export default function TextForm(props) {
+    const handleUpClick = ()=>{
+        // console.log("Uppercase was clicked" + text);
+        let newText = text.toUpperCase();
+        setText(newText)
+        props.showAlert("Converted to uppercase!", "success");
+    }
+
+    const handleLoClick = ()=>{
+      // console.log("Uppercase was clicked" + text);
+      let newText = text.toLowerCase();
+      setText(newText)
+      props.showAlert("Converted to lowercase!", "success");
+  }
+
+    const handleOnChange = (event)=>{
+        // console.log("On change");
+        setText(event.target.value)
+        
+    }
+    const handleClearClick = () => {
+      setText(""); // Clears the text
+      props.showAlert("Text cleared!", "success");
+  };
+
+  const handleRemoveSpaces = () => {
+    let newText = text.replace(/\s+/g, ' ').trim(); // Removes extra spaces
+    setText(newText);
+    props.showAlert("Extra spaces get removed!", "success");
+};
+
+  
+
+    const [text, setText] = useState('Enter text here');
+  return (
+    <>
+    <div className="container"style={{color: props.mode==='dark'?'white':'#042743'}}>
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+        <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor: props.mode==='dark'?'grey':'white', color: props.mode==='dark'?'white':'#042743'}}
+        id="myBox" rows="8"></textarea>
+    </div>
+  <button className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to Uppercase</button>
+  <button className="btn btn-primary mx-2" onClick={handleLoClick}>Convert to Lowercase</button>
+  <button className="btn btn-primary mx-2 my-2" onClick={handleClearClick}>Clear Text</button>
+  <button className="btn btn-primary mx-2" onClick={handleRemoveSpaces}>Remove Extra Spaces</button>
+    </div>
+<div className="container my-3" style={{color: props.mode==='dark'?'white':'#042743'}}>
+  <h4>Your text summary</h4>
+  <p>{text.split(" ").length} words and {text.length} characters</p>
+  <p>{0.008 * text.split(" ").length} Minutes read</p>
+  <h2>Preview</h2>
+  <p>{text}</p>
+</div>
+    </>
+  )
+}
